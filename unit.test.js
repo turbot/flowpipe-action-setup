13105ebd6a7a1b7f11f4931963ad24052da38902
Omero Saienni <omerosaienni@hotmail.com>
2024-01-11 13:44:10 +1300

    Separated tests

--- unit.test.js
+++ unit.test.js
@@ -60,49 +60,6 @@ describe("checkPlatform", () => {
   });
 });
 
-describe("getFlowpipeReleases Integration", () => {
-  it("should fetch data from GitHub API and limit the returned results", async () => {
-    const data = await getFlowpipeReleases(1, 5);
-
-    // Data check
-    expect(Array.isArray(data)).toBeTruthy();
-    expect(data.length).toBe(5);
-
-    // Data structure check
-    if (data.length > 0) {
-      expect(data[0]).toHaveProperty("url");
-      expect(data[0]).toHaveProperty("tag_name");
-    }
-  });
-
-  it("should fetch data from GitHub API and not limit the returned results", async () => {
-    const data = await getFlowpipeReleases(1);
-
-    // Data check
-    expect(Array.isArray(data)).toBeTruthy();
-    expect(data.length).toBeGreaterThan(5);
-
-    // Data structure check
-    if (data.length > 0) {
-      expect(data[0]).toHaveProperty("url");
-      expect(data[0]).toHaveProperty("tag_name");
-    }
-  });
-
-  it("should fetch data from GitHub API and not page", async () => {
-    const data = await getFlowpipeReleases(5, 5);
-
-    // Data check
-    expect(Array.isArray(data)).toBeTruthy();
-    expect(data.length).toBeGreaterThan(5);
-
-    // Data structure check
-    if (data.length > 0) {
-      expect(data[0]).toHaveProperty("url");
-      expect(data[0]).toHaveProperty("tag_name");
-    }
-  });
-});
 
 // NOT MINE
 
